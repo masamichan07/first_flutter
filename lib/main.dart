d13138d095c719d496116ce135f3d6063d3aeff9
masamichan <positivemasa07@gmail.com>
2025-05-25 10:01:14 +0900

    prepare for the coding

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Text('Hello Flutter', textDirection: TextDirection.ltr));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(centerTitle: true, title: Text('Home Page')),
+        body: Center(
+          child: Text(
+            'Hello, Flutter',
+            style: TextStyle(fontSize: 30.0),
+            textDirection: TextDirection.ltr,
+          ),
+        ),
+      ),
+    ),
+  );
 }
